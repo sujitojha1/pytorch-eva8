82a03d5a85047bb200f55a637e64addfb69e8337
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-11 04:27:31 +0530

    minor changes

--- utils.py
+++ utils.py
@@ -390,3 +390,38 @@ class GradCAM:
 
     def __call__(self, input, class_idx=None, retain_graph=False):
         return self.forward(input, class_idx, retain_graph)
+
+
+def calAccuracy(net, dataloader, device):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            images,labels = images.to(device),labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the  train images: {(100 * correct / total)} %%')
+
+def calClassAccuracy(net, dataloader, classes, device):
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            images,labels = images.to(device),labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
