7b7bfaec1a8d8c677a5f78539898efb26171042b
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-11 02:56:21 +0530

    minor changes

--- utils.py
+++ utils.py
@@ -119,3 +119,33 @@ def unnormalize(img):
          img[i] = (img[i]*channel_stdevs[i])+channel_means[i]
   
     return np.transpose(img, (1,2,0))
+
+# define a function to plot misclassified images
+def plot_misclassified_images(model, test_loader, classes, device):
+    # set model to evaluation mode
+    model.eval()
+
+    misclassified_images = []
+    actual_labels = []
+    predicted_labels = []
+    
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            _, pred = torch.max(output, 1)
+            for i in range(len(pred)):
+                if pred[i] != target[i]:
+                    misclassified_images.append(data[i])
+                    actual_labels.append(classes[target[i]])
+                    predicted_labels.append(classes[pred[i]])
+
+    # Plot the misclassified images
+    fig = plt.figure(figsize=(12, 5))
+    for i in range(10):
+        sub = fig.add_subplot(2, 5, i+1)
+        npimg = unnormalize(misclassified_images[i].cpu())
+        plt.imshow(npimg, cmap='gray', interpolation='none')
+        sub.set_title("Actual: {}, Pred: {}".format(actual_labels[i], predicted_labels[i]),color='red')
+    plt.tight_layout()
+    plt.show()
