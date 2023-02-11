ebfcea7638e5bb7c8a41a5d17c05b1dfc76ca8fd
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-11 21:15:34 +0530

    minor change

--- utils.py
+++ utils.py
@@ -309,42 +309,42 @@ def find_resnet_layer(arch, target_layer_name):
     return target_layer
 
 
-# def denormalize(tensor, mean, std):
-#     if not tensor.ndimension() == 4:
-#         raise TypeError('tensor should be 4D')
+def denormalize1(tensor, mean, std):
+    if not tensor.ndimension() == 4:
+        raise TypeError('tensor should be 4D')
 
-#     mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
-#     std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
 
-#     return tensor.mul(std).add(mean)
-
-
-# def normalize(tensor, mean, std):
-#     if not tensor.ndimension() == 4:
-#         raise TypeError('tensor should be 4D')
-
-#     mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
-#     std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
-
-#     return tensor.sub(mean).div(std)
+    return tensor.mul(std).add(mean)
 
 
-# class Normalize(object):
-#     def __init__(self, mean, std):
-#         self.mean = mean
-#         self.std = std
+def normalize1(tensor, mean, std):
+    if not tensor.ndimension() == 4:
+        raise TypeError('tensor should be 4D')
 
-#     def __call__(self, tensor):
-#         return self.do(tensor)
+    mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
 
-#     def do(self, tensor):
-#         return normalize(tensor, self.mean, self.std)
+    return tensor.sub(mean).div(std)
 
-#     def undo(self, tensor):
-#         return denormalize(tensor, self.mean, self.std)
 
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        return self.do(tensor)
+
+    def do(self, tensor):
+        return normalize1(tensor, self.mean, self.std)
+
+    def undo(self, tensor):
+        return denormalize1(tensor, self.mean, self.std)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
 
 
 # class GradCAM:
