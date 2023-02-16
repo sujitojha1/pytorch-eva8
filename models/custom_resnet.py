fc8c8840a5b0b742835e8d5ff3f7e582765f6c7a
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-16 23:48:58 +0530

    minor changes

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -41,14 +41,15 @@ class CustomResNet(nn.Module):
             nn.ReLU(),
         )
 
-        self.layer1 = self._make_layer(64, num_blocks[0], stride=1)
+        self.layer1 = self._make_layer(64, 128, stride=1)
         self.layer2 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3,
+            nn.Conv2d(128, 256, kernel_size=3,
                                stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
         )
-        self.layer3 = self._make_layer(256, num_blocks[2], stride=2)
+        self.layer3 = self._make_layer(256, 512, stride=1)
 
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
@@ -65,7 +66,6 @@ class CustomResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
