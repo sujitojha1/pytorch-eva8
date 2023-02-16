bfcdeb0bc59c54f78de47325a661e1b1cdf3185e
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-16 23:00:29 +0530

    minorchanges

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 # Define the ResBlock class
 class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, out_channels, stride=1):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(ResBlock, self).__init__()
 
         self.resConv = nn.Sequential(
@@ -27,29 +27,6 @@ class ResBlock(nn.Module):
     def forward(self, x):
         out = self.resConv(x)
         out += self.shortcut(x)
-        return out
-
-# Define the ResBlock class
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-    
-    def forward(self, x):
-        out = self.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = self.relu(out)
         return out
 
 # Define a function to create ResNet layers
