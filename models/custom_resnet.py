e728bf42fde71a6d0f86d3bcf3177a8a9e35be35
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-16 23:32:23 +0530

    monor change

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -31,17 +31,17 @@ class ResBlock(nn.Module):
 
 
 class CustomResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self,in_channels, out_channels, num_blocks, stride):
@@ -62,43 +62,3 @@ class CustomResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
-
-# Define a function to create ResNet layers
-def make_resnet_layer(in_channels, out_channels, num_blocks, stride):
-    strides = [stride] + [1] * (num_blocks - 1)
-    layers = []
-    for stride in strides:
-        layers.append(ResBlock(in_channels, out_channels, stride))
-        in_channels = out_channels
-    return nn.Sequential(*layers)
-
-# Define the custom ResNet architecture
-class CustomResNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(CustomResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(3, 2, 1)
-        self.layer1 = make_resnet_layer(64, 64, 2, 1)
-        self.layer2 = make_resnet_layer(64, 128, 2, 2)
-        self.layer3 = make_resnet_layer(128, 256, 2, 2)
-        self.layer4 = make_resnet_layer(256, 512, 2, 2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512, num_classes)
-    
-    def forward(self, x):
-        out = self.relu(self.bn1(self.conv1(x)))
-        out = self.maxpool(out)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.avgpool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-
-
-
-
