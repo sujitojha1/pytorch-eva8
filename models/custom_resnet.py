bde0b17c3cd574abe27ce60ae8dcbbf1785b630b
Sujit Kumar Ojha <sujit.ojha@gmail.com>
2023-02-24 19:03:44 +0530

    revert back

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -69,8 +69,6 @@ class CustomResNet(nn.Module):
 
         self.linear = nn.Linear(512, num_classes)
 
-        self.softmaxLayer = nn.LogSoftmax(dim=1)
-
     def forward(self, x):
         out = self.prep_layer(x)
         out = self.layer1(out)
@@ -78,5 +76,5 @@ class CustomResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = out.view(out.size(0), -1)
-        out = self.softmaxLayer(self.linear(out))
+        out = self.linear(out)
         return out
